b7cafa82e050c425e5b6a1d8a1836e3d0ad56f50
Байрамгулов Мунир Ниязович <mbrmglv@mts.ru>
2025-11-02 16:15:50 +0300

    test api

--- src/main/java/com/example/organization/config/WebSocketConfig.java
+++ src/main/java/com/example/organization/config/WebSocketConfig.java
@@ -23,7 +23,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins(allowedOrigins)
+                .setAllowedOrigins("https://se.ifmo.ru")
                 .withSockJS();
     }
 }
